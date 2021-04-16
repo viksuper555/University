fd561bf6ebf0a30ea9fe7598caba05c1eab0149b
Viktor Kolev <viksuper555@gmail.com>
2021-04-16 23:10:12 +0300

    Finished my course project structure

---
+++ MusicProject/Album.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicProject
+{
+    public class Album
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public List<Song> Songs { get; set; } = new List<Song>();
+
+        public List<Artist> Artists { get; set; } = new List<Artist>();
+
+        private Popularity popularity;
+   
+        public List<Song> GetSongs()
+        {
+            return Songs;
+        }
+
+        public string GetCreators()
+        {
+            return string.Join(", ", this.Artists);
+        }
+    
+        public Song GetSong(string name)
+        {
+            return this.Songs.FirstOrDefault(x => x.Name == name);
+        }
+
+        public string GetPopularity()
+        {
+            return popularity.ToString();
+        }
+
+        public List<string> GetSongGenres()
+        {
+            return Songs.Select(x => x.Genre).Distinct().ToList();
+        }
+
+        public void AddSong(Song song)
+        {
+            Songs.Add(song);
+        }
+
+        public void DeleteSong(Song song)
+        {
+            var oldSong = Songs.FirstOrDefault(x => x.Name == song.Name);
+            Songs.Remove(oldSong);
+        }
+
+        public void UpdateSong(Song song)
+        {
+            for (int i = 0; i < Songs.Count; i++)
+            {
+                if(Songs[i].Name == song.Name)
+                {
+                    Songs[i] = song;
+                    break;
+                }    
+            }
+        }
+        
+    }
+
+    public enum Popularity
+    {
+        Dead,
+        Frozen,
+        Chilly,
+        Cold,
+        Cool,
+        Warm,
+        HOT
+    }
+}
---
+++ MusicProject/Artist.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicProject
+{
+    public class Artist : Creator, ICreator
+    {
+        public string BirthName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Nationality { get; set; }
+
+        private int? YearOfDeath { get; set; }
+
+        public void AddAlbum(Album album)
+        {
+            this.Albums.Add(album);
+        }
+
+        public void DeleteAlbum(string name)
+        {
+            var album = this.Albums.FirstOrDefault(x => x.Name == name);
+            this.Albums.Remove(album);
+        }
+
+        public Album GetAlbum(string name) => this.Albums.FirstOrDefault(x => x.Name == name);
+
+        public List<Album> GetAlbums() 
+        {
+            //I know it's unnecessary
+            var albums = from a in this.Albums
+                         select a;
+
+            return albums.ToList();
+        }
+        public List<Song> GetSingles() => this.Singles;
+
+        public Song GetSong(string name) => this.Singles.FirstOrDefault(x => x.Name == name);
+
+        public void UpdateAlbum(Album album)
+        {
+            for (int i = 0; i < Albums.Count; i++)
+            {
+                if (Albums[i].Name == album.Name)
+                {
+                    Albums[i] = album;
+                    break;
+                }
+            }
+        }
+
+        public override string GetStatus()
+        {
+            if (YearOfDeath == null)
+                return "Alive";
+            else
+                return "Inactive since " + YearOfDeath;
+        }
+    }
+}
